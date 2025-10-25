a1937acb02603b6f115885869ebc68ffb79c1394
mr-devloper005 <mr.opxoo5@gmail.com>
2025-10-26 03:14:37 +0530

    removed tsx

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,4 +1,4 @@
-import { UsersDataTable } from "./components/users_data_table.tsx";
+import { UsersDataTable } from "./components/users_data_table";
 
 export default function UsersPage() {
   return (
